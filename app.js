511bbd749539f3e353ced1ee2986976de161745b
OM GUPTA <omgupta0312@gmail.com>
2023-01-05 17:41:12 +0530

    Added Authentication Feature

--- app.js
+++ app.js
@@ -7,14 +7,16 @@ const Joi = require('joi')
 const cors = require("cors")
 const session = require('express-session')
 const flash = require('connect-flash')
-
+const passport = require('passport')
+const localStrategy = require('passport-local')
 const ExpressError = require("./utils/ExpressError")
+const User = require('./models/user')
 
 
 //routes-------------------------------
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
-
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+const userRoutes = require('./routes/users')
 
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -27,62 +29,72 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     .catch((err) => {
         console.log("ERROR!!")
         console.log(err);
-    })
+    });
 
 
 
 const app = express();
-app.engine('ejs', ejsMate)
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 
-app.use(cors())
-app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname ,'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 const sessionConfig = {
-    secret:'thisshouldbeabettersecret!',
-    resave:false,
-    saveUninitialized:true,
-    cookie:{
-        httpOnly:true,
-        expires:Date.now() + 1000*60*60*24*7,
-        maxAge:1000*60*60*24*7
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
     }
-}
+};
 
-app.use(session(sessionConfig))
-app.use(flash())
+app.use(session(sessionConfig));
+app.use(flash());
 
-app.use((req,res,next)=>{
-    res.locals.success=req.flash('success');
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
-})
+});
 
-app.use('/campgrounds',campgrounds)
-app.use('/campgrounds/:id/reviews',reviews)
 
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 
 app.get('/', (req, res) => {
     // res.send('Hello From Yelp Camp')
     res.render('home')
-})
+});
+
 app.all("*", (req, res, next) => {
     // res.send("404!!")
     next(new ExpressError('Page Not Found', 404));
 
-})
+});
 
 app.use((err, req, res, next) => {
     const { statusCode = 500, message = "Something went wrong" } = err;
-    if(!err.message) err.message="Something went wrong";
-    res.status(statusCode).render('error',{err})
-})
+    if (!err.message) err.message = "Something went wrong";
+    res.status(statusCode).render('error', { err })
+});
 
 app.listen(3000, () => {
     console.log('Listening on port 3000')
-})
+});
