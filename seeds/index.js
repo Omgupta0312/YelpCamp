1dc12aacd35d6b54a9779b34f2d2def4e3d2a144
OM GUPTA <omgupta0312@gmail.com>
2023-01-08 16:25:28 +0530

    Clusturing in Maps Done

--- seeds/index.js
+++ seeds/index.js
@@ -22,36 +22,37 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 200; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() *50)+10;
+        const price = Math.floor(Math.random() * 50) + 10;
         const camp = new Campground({
             author: '63b70b5b20c1a730233c4244',
             location: `${cities[random1000].city},${cities[random1000].state}`,
 
             title: `${sample(descriptors)} ${sample(places)}`,
 
-            description:"Learning a little each day adds up. Research shows that students who make learning a habit are more likely to reach their goals. Set time aside to learn and get reminders using your learning scheduler.",
-            
+            description: "Learning a little each day adds up. Research shows that students who make learning a habit are more likely to reach their goals. Set time aside to learn and get reminders using your learning scheduler.",
+
             geometry: {
                 type: "Point",
-                coordinates: [-113.1331, 47.0202]
+                coordinates: [cities[random1000].longitude,
+                cities[random1000].latitude,]
             },
 
-            images:[
+            images: [
                 {
-                  url: 'https://res.cloudinary.com/dyjhgujaa/image/upload/v1673012654/YelpCamp/la0rzqveiuficaqj8yhy.jpg',
-                  filename: 'YelpCamp/la0rzqveiuficaqj8yhy'
+                    url: 'https://res.cloudinary.com/dyjhgujaa/image/upload/v1673012654/YelpCamp/la0rzqveiuficaqj8yhy.jpg',
+                    filename: 'YelpCamp/la0rzqveiuficaqj8yhy'
                 },
                 {
-                  url: 'https://res.cloudinary.com/dyjhgujaa/image/upload/v1673012665/YelpCamp/avgiczdnaoc2mrm3a8ym.jpg',
-                  filename: 'YelpCamp/avgiczdnaoc2mrm3a8ym'
+                    url: 'https://res.cloudinary.com/dyjhgujaa/image/upload/v1673012665/YelpCamp/avgiczdnaoc2mrm3a8ym.jpg',
+                    filename: 'YelpCamp/avgiczdnaoc2mrm3a8ym'
                 },
                 {
-                  url: 'https://res.cloudinary.com/dyjhgujaa/image/upload/v1673012666/YelpCamp/q7fprxkys0f6rtiq70fs.jpg',
-                  filename: 'YelpCamp/q7fprxkys0f6rtiq70fs'
+                    url: 'https://res.cloudinary.com/dyjhgujaa/image/upload/v1673012666/YelpCamp/q7fprxkys0f6rtiq70fs.jpg',
+                    filename: 'YelpCamp/q7fprxkys0f6rtiq70fs'
                 }
-              ],
+            ],
 
             price
         })
